4fd82a0d42d644284df1d72c26226d4f8217eec8
Mez Pahlan <mez.pahlan@gmail.com>
2016-07-20 20:32:22 +0100

    Use Retrofit to fetch an RSS feed from the Boston Globe Big picture website

--- app/src/main/java/uk/co/mezpahlan/thedroidpicture/MainActivity.java
+++ app/src/main/java/uk/co/mezpahlan/thedroidpicture/MainActivity.java
@@ -1,7 +1,12 @@
 package uk.co.mezpahlan.thedroidpicture;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BostonGlobeClient client = BostonGlobeServiceGenerator.createService(BostonGlobeClient.class);
+
+        Call<RssFeed> call = client.getRssFeed();
+
+        call.enqueue(new Callback<RssFeed>() {
+            @Override
+            public void onResponse(Call<RssFeed> call, Response<RssFeed> response) {
+                if (response.isSuccessful()) {
+                    Log.d("Mez", response.body().toString());
+                } else {
+                    Log.e("Mez", response.errorBody().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RssFeed> call, Throwable t) {
+                Log.e("Mez", t.getMessage());
+            }
+        });
     }
 }
