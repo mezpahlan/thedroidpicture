11c9a726a3de2a7e829bd10af89152722da9f400
Mez Pahlan <mez.pahlan@gmail.com>
2016-07-27 21:34:13 +0100

    Initial Item Fragment and Layout

---
+++ app/src/main/java/uk/co/mezpahlan/thedroidpicture/rss/item/ItemFragment.java
@@ -0,0 +1,18 @@
+package uk.co.mezpahlan.thedroidpicture.rss.item;
+
+import android.app.Fragment;
+
+/**
+ * Created by mpahlan on 27/07/16.
+ */
+public class ItemFragment extends Fragment {
+
+    public ItemFragment() {
+        // Required empty constructor
+    }
+
+    public static ItemFragment newInstance() {
+        return new ItemFragment();
+    }
+
+}
