5f394759fc655ea4efeea9047149b2027e8f42b3
Mez Pahlan <mez.pahlan@gmail.com>
2016-07-27 21:31:35 +0100

    Initial ItemMVP interface

---
+++ app/src/main/java/uk/co/mezpahlan/thedroidpicture/rss/item/ItemMVP.java
@@ -0,0 +1,21 @@
+package uk.co.mezpahlan.thedroidpicture.rss.item;
+
+import uk.co.mezpahlan.thedroidpicture.base.BaseMvp;
+
+/**
+ * Created by mpahlan on 27/07/16.
+ */
+public interface ItemMVP {
+
+    interface View extends BaseMvp.View {
+
+    }
+
+    interface Presenter {
+
+    }
+
+    interface ModelInteractor extends BaseMvp.ModelInteractor {
+
+    }
+}
