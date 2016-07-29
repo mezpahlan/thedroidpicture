8f3eb6edb386c3a706bca96b13b1fe8a221a258e
Mez Pahlan <mez.pahlan@gmail.com>
2016-07-30 00:13:55 +0100

    Since we've got rid of the repository pattern, we now implement the use case in the Model Interactor

--- app/src/main/java/uk/co/mezpahlan/thedroidpicture/rss/feed/FeedModelInteractor.java
+++ app/src/main/java/uk/co/mezpahlan/thedroidpicture/rss/feed/FeedModelInteractor.java
@@ -1,34 +1,61 @@
 package uk.co.mezpahlan.thedroidpicture.rss.feed;
 
+import android.util.Log;
+
 import java.util.List;
 
-import uk.co.mezpahlan.thedroidpicture.data.BostonGlobe;
-import uk.co.mezpahlan.thedroidpicture.data.RssRepository;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import uk.co.mezpahlan.thedroidpicture.data.BostonGlobeClient;
+import uk.co.mezpahlan.thedroidpicture.data.BostonGlobeServiceGenerator;
 import uk.co.mezpahlan.thedroidpicture.data.model.RssFeed;
 
 /**
  * Created by mpahlan on 24/07/16.
  */
 public class FeedModelInteractor implements FeedMvp.ModelInteractor {
-
     private FeedMvp.Presenter feedPresenter;
-    private RssRepository repository;
+    private BostonGlobeClient client;
 
     public FeedModelInteractor(FeedMvp.Presenter feedPresenter) {
         this.feedPresenter = feedPresenter;
-
-        // Using DI select the correct repository??
-        repository = new BostonGlobe(this);
+        client = BostonGlobeServiceGenerator.createService(BostonGlobeClient.class);
     }
 
     @Override
     public void fetch() {
-        repository.loadFeed(this);
+        Call<RssFeed> call = client.getRssFeed();
+
+        call.enqueue(new Callback<RssFeed>() {
+            @Override
+            public void onResponse(Call<RssFeed> call, Response<RssFeed> response) {
+                if (response.isSuccessful()) {
+                    // We just want the response. Don't do any conversions here.
+                    onFetched(response.body());
+                } else {
+                    // We just want the error. Don't do any conversions here.
+                    onError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RssFeed> call, Throwable t) {
+                // We just want the error. Don't do any conversions here.
+                onError();
+            }
+        });;
     }
 
     @Override
     public void onFetched(RssFeed rssFeed) {
         List<RssFeed.Item> rssItems = rssFeed.getChannel().getItem();
         feedPresenter.onLoadSuccess(rssItems);
+    }
+
+    @Override
+    public void onError() {
+        // TODO: Implement me
+        Log.e("Mez", "Something went wrong in the FeedModelInteractor");
     }
 }
