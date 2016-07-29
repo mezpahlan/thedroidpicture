01b765bdc07401ce529d4b5af4d13006a11bfa62
Mez Pahlan <mez.pahlan@gmail.com>
2016-07-30 00:11:09 +0100

    Add getRssItem client method

--- app/src/main/java/uk/co/mezpahlan/thedroidpicture/data/BostonGlobeClient.java
+++ app/src/main/java/uk/co/mezpahlan/thedroidpicture/data/BostonGlobeClient.java
@@ -1,13 +1,19 @@
 package uk.co.mezpahlan.thedroidpicture.data;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Url;
 import uk.co.mezpahlan.thedroidpicture.data.model.RssFeed;
 
 /**
  * Created by mpahlan on 19/07/16.
  */
 public interface BostonGlobeClient {
+
     @GET("rss/bigpicture")
     Call<RssFeed> getRssFeed();
+
+    @GET
+    Call<ResponseBody> getRssItem(@Url String itemUrl);
 }
