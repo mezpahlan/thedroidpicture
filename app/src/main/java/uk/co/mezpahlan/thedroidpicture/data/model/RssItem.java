299b32be810502e05c908b3cf468fc30b6e82a76
Mez Pahlan <mez.pahlan@gmail.com>
2016-07-30 23:29:42 +0100

    Add a constructor to RssItem class too so that we can do a { get; private set;} thing.

--- app/src/main/java/uk/co/mezpahlan/thedroidpicture/data/model/RssItem.java
+++ app/src/main/java/uk/co/mezpahlan/thedroidpicture/data/model/RssItem.java
@@ -6,27 +6,25 @@ import java.util.List;
  * Created by mpahlan on 28/07/16.
  */
 public class RssItem {
-
     private String headline;
     private String teaseText;
     private String byline;
     private String date;
     private List<Photo> photos;
 
+    public RssItem(String headline, String teaseText, String byline, String date, List<Photo> photos) {
+        this.headline = headline;
+        this.teaseText = teaseText;
+        this.byline = byline;
+        this.date = date;
+        this.photos = photos;
+    }
+
     public String getHeadline() { return headline; }
-    public void setHeadline(String headline) { this.headline = headline; }
-
     public String getTeaseText() { return teaseText; }
-    public void setTeaseText(String teaseText) { this.teaseText = teaseText; }
-
     public String getByline() { return byline; }
-    public void setByline(String byline) { this.byline = byline; }
-
     public String getDate() { return date; }
-    public void setDate(String date) { this.date = date; }
-
     public List<Photo> getPhotos() { return photos; }
-    public void setPhotos(List<Photo> photos) { this.photos = photos; }
 
     public static class Photo {
         private String imageLink;
