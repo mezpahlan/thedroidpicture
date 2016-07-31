bfa2823b18e5cedf000bc37ce709c0484d4bb843
Mez Pahlan <mez.pahlan@gmail.com>
2016-07-31 22:43:54 +0100

    Collapse whitespace

--- app/src/main/java/uk/co/mezpahlan/thedroidpicture/rss/feed/FeedMvp.java
+++ app/src/main/java/uk/co/mezpahlan/thedroidpicture/rss/feed/FeedMvp.java
@@ -11,33 +11,21 @@ import uk.co.mezpahlan.thedroidpicture.data.model.RssFeed;
  * Created by mpahlan on 23/07/16.
  */
 public interface FeedMvp {
-
     interface View extends BaseMvp.LCEView <List<RssFeed.Item>> {
-
         // Only what the view is concerned with........ typically showing things?
         void showRssItem(String title, String rssItemLink);
-
     }
 
     interface Presenter {
-
         void load(boolean isUserDrive);
-
         void onLoadSuccess(@NonNull List<RssFeed.Item> rssList);
-
         void onLoadError();
-
-        void onSelectRssItem(@NonNull RssFeed.Item rssItem);
-
+        void onSelectPhoto(@NonNull RssFeed.Item rssItem);
     }
 
     interface ModelInteractor {
-
         void fetch();
-
         void onFetched(RssFeed rssFeed);
-
         void onError();
     }
-
 }
