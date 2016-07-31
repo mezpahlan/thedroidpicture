41aa56a9e4da0b590eab0cbf1b88d0bc5bdf9aa8
Mez Pahlan <mez.pahlan@gmail.com>
2016-07-31 14:20:26 +0100

    Swap StaggeredLayoutManager for LinearLayoutManger in Feed Fragment

--- app/src/main/java/uk/co/mezpahlan/thedroidpicture/rss/feed/FeedFragment.java
+++ app/src/main/java/uk/co/mezpahlan/thedroidpicture/rss/feed/FeedFragment.java
@@ -6,8 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -67,7 +67,7 @@ public class FeedFragment extends Fragment implements FeedMvp.View {
         recyclerView.setAdapter(listAdapter);
 
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, 1));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         // Pull-to-refresh
         SwipeRefreshLayout swipeRefreshLayout =
