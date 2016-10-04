1753a0cb72ba4587deccf1826e7ff9f9649c017f
Mez Pahlan <mez.pahlan@gmail.com>
2016-10-04 18:40:51 +0100

    Swap the default ActionBar for a ToolBar in the Feed view.

--- app/src/main/java/uk/co/mezpahlan/thedroidpicture/rss/feed/FeedActivity.java
+++ app/src/main/java/uk/co/mezpahlan/thedroidpicture/rss/feed/FeedActivity.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import uk.co.mezpahlan.thedroidpicture.R;
 
@@ -14,6 +15,9 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rss_feed);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         if (null == savedInstanceState) {
             initFragment(FeedFragment.newInstance());
